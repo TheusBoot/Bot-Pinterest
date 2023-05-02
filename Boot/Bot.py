38b709c07cd6c9a8f004817b937f6fbfe263fc7d
Matheus Trindade <emailmartingcf1@gmail.com>
2023-05-02 02:20:18 -0300

    primeiros comandos.

---
+++ Boot/Bot.py
@@ -0,0 +1,14 @@
+from time import sleep
+from time import choice
+
+class Pinterest:
+    def __init__(self, user_name,password) -> None:
+        """Aqui vai sua senha do Pinterest, atenção, o mesmo não pode ter autéticação de 2 fatores"""
+        self.user_name = user_name
+        self.user_passowrd = password
+
+    def upload_pin(self):
+        pass
+
+    
+    
